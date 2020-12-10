9e532bf62f0fa855724eb1e607ade17cfc9acf99
Volodymyr Dmytrenko <volodymyr.dmytrenko@p2h.com>
2020-12-10 23:05:06 +0200

    fist_lesson_on_webpack

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
-const post = new Post('Webpack Post Title');
-console.log('Post to string:', post.toString());
+import * as $ from 'jquery';
+import '@css/style.css';
+import Post from '@model/post';
+import json from '@assets/data';
+import webpackLogo from '@assets/icon-square-big.png';
+import xml from '@assets/data.xml';
+import csv from '@/assets/data.csv';
+
+const post = new Post('Webpack Post Title', webpackLogo);
+
+$('pre').html(post.toString());
+console.log('JSON:', json);
+console.log('XML:', xml);
+console.log('CSV:', csv);
--- src/statistics.js
+++ src/statistics.js
@@ -1,15 +1,15 @@
+import * as $ from 'jquery';
 function createStatistics() {
   let counter = 0;
   let isDestroyed = false;
   const listener = () => counter++;
-  document.addEventListener('click', listener);
+  $(document).on('click', listener);
   return {
     destroy() {
-      document.removeEventListener('click', listener);
-      isDestroyed = true;
+      $(document).off('click', listener); isDestroyed = true;
     },
     getClicks() {
-      if (isDestroyed) return 'Statistics is destroyed';
+      if (isDestroyed) return 'Statistics is fully destroyed.';
       return counter;
     }
   };
